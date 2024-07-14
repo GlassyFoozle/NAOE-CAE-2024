6f53b81e92c2f3911454f72ffae57db2fdfa33b2
Chaemin <brian2562@gmail.com>
2024-07-15 01:04:15 +0900

    Added GUI.ui and changed GUI.py to be based on ui file

--- GUI.py
+++ GUI.py
@@ -1,33 +1,22 @@
 import sys
-import io
 import folium
-from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QLineEdit, QPushButton, QLabel
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+import io
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
 
-class MyApp(QWidget):
+# Load UI file
+form_class = uic.loadUiType("GUI.ui")[0]
+
+
+class WindowClass(QDialog, form_class) :
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Test Map')
-        self.window_width, self.window_height = 1200, 800
-        self.setMinimumSize(self.window_width, self.window_height)
+        self.setupUi(self)
+        self.setWindowTitle('CAE Project')
+        self.window_width, self.window_height = 1400, 800
+        self.setMinimumSize(400, 300)
 
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
-
-        # Input fields and button
-        input_layout = QHBoxLayout()
-        self.lat_input = QLineEdit(self)
-        self.lon_input = QLineEdit(self)
-        self.add_marker_button = QPushButton('Add Marker', self)
-
-        input_layout.addWidget(QLabel('Latitude:'))
-        input_layout.addWidget(self.lat_input)
-        input_layout.addWidget(QLabel('Longitude:'))
-        input_layout.addWidget(self.lon_input)
-        input_layout.addWidget(self.add_marker_button)
-
-        self.layout.addLayout(input_layout)
-
+        # Load folium map
         self.coordinate = (35.532600, 127.524612)
         self.map = folium.Map(
             title="Start Point",
@@ -37,26 +26,26 @@ class MyApp(QWidget):
 
         self.data = io.BytesIO()
         self.map.save(self.data, close_file=False)
+        self.webEngineView.setHtml(self.data.getvalue().decode())
 
-        self.webView = QWebEngineView()
-        self.webView.setHtml(self.data.getvalue().decode())
-        self.layout.addWidget(self.webView)
-
-        self.add_marker_button.clicked.connect(self.update_map)
-
+    # Update map and accident cases
     def update_map(self):
+        # 현재 위치 아이콘
+        icon_url = 'https://cdn4.iconfinder.com/data/icons/web-ui-color/128/Marker_red-512.png'
+        ship_icon = folium.features.CustomIcon(icon_url, icon_size=(30, 30))
+
         lat = float(self.lat_input.text())
         lon = float(self.lon_input.text())
 
         self.coordinate = (lat, lon)
         self.map = folium.Map(
             title="Start Point",
-            zoom_start=8,
+            zoom_start=11,
             location=self.coordinate
         )
 
         # Add new marker
-        folium.Marker(location=self.coordinate).add_to(self.map)
+        folium.Marker(location=self.coordinate, icon=ship_icon).add_to(self.map)
 
         # Save updated map to data object
         self.data = io.BytesIO()
@@ -65,18 +54,16 @@ class MyApp(QWidget):
         # Update web view with new map
         self.webView.setHtml(self.data.getvalue().decode())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__" :
+    # QApplication : 프로그램을 실행시켜주는 클래스
     app = QApplication(sys.argv)
-    app.setStyleSheet('''
-        QWidget {
-            font-size: 20px;
-        }
-    ''')
 
-    myApp = MyApp()
-    myApp.show()
+    # WindowClass의 인스턴스 생성
+    myWindow = WindowClass()
 
-    try:
-        sys.exit(app.exec_())
-    except SystemExit:
-        print('Closing Window...')
+    # 프로그램 화면을 보여주는 코드
+    myWindow.show()
+
+    # 프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
+    app.exec_()
